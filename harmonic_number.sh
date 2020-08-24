bb5178eb558d6c7a664abc0c1c20e2c533cdc70d
Susnigdha De <susnigdha.de@gmail.com>
2020-08-24 15:44:08 +0530

    Write a program that takes a command-line argument n and prints the nth harmonic number.

---
+++ harmonic_number.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+#Write a program that takes a command-line argument n and prints the nth harmonic number.
+
+Hn=0
+read -p "Enter the limit" n
+
+for((i=1;i<=n;i++))
+do
+echo $((1/$i))
+Hn=$(($Hn+1/$i))
+done
+
+echo Harmonic Number is $Hn
+
